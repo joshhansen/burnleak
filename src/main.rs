8aa9521f625334832b85d1a068efed8083beb499
Josh Hansen <git@joshhansen.tech>
2024-07-19 17:00:22 -0700

    Eliminate dropout, leak persists

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use burn::{
     backend::{wgpu::WgpuDevice, Wgpu},
     nn::{
         conv::{Conv2d, Conv2dConfig},
-        Dropout, DropoutConfig, Linear, LinearConfig,
+        Linear, LinearConfig,
     },
     tensor::{
         activation::{relu, sigmoid},
@@ -16,13 +16,8 @@ const POSSIBLE_DIRECTIONS: usize = 8;
 
 const POSSIBLE_UNIT_TYPES: usize = 10;
 
-/// How many unit types there are, counting city as a unit type
-const POSSIBLE_UNIT_TYPES_WRIT_LARGE: usize = POSSIBLE_UNIT_TYPES + 1;
-
 #[allow(non_camel_case_types)]
 type fX = f32;
-
-const P_DROPOUT: f64 = 0.4;
 
 const POSSIBLE_CITY_ACTIONS: usize = POSSIBLE_UNIT_TYPES; // all possible productions
 
@@ -30,11 +25,6 @@ const POSSIBLE_UNIT_ACTIONS: usize = POSSIBLE_DIRECTIONS + 2; // plus skip and d
 
 const POSSIBLE_ACTIONS: usize = POSSIBLE_CITY_ACTIONS + POSSIBLE_UNIT_ACTIONS;
 
-const ADDED_WIDE_FEATURES: usize = 13;
-
-/// Number of 1d (wide) features
-/// Includes `POSSIBLE_UNIT_TYPES` twice: once for the unit type one-hot-encoded, once for the overall unit counts, plus one for city
-const WIDE_LEN: usize = POSSIBLE_UNIT_TYPES_WRIT_LARGE + POSSIBLE_UNIT_TYPES + ADDED_WIDE_FEATURES;
 const DEEP_WIDTH: usize = 15;
 const DEEP_HEIGHT: usize = 15;
 const DEEP_TILES: usize = DEEP_WIDTH * DEEP_HEIGHT;
@@ -53,24 +43,14 @@ const PER_ACTION_CHANNELS: usize = 1;
 /// Total length of convolution output after reducing
 const DEEP_OUT_LEN: usize = DEEP_OUT_TILES * POSSIBLE_ACTIONS * PER_ACTION_CHANNELS;
 
-/// Total length of the feature vectors that are input to the dnn
-const FEATS_LEN: usize = WIDE_LEN + DEEP_IN_LEN;
-
 struct AgzActionModel<B: Backend> {
-    dropout: Dropout,
     convs: Vec<Conv2d<B>>,
     dense_common: Vec<Linear<B>>,
     dense_per_action: Vec<Vec<Linear<B>>>,
 }
 
 impl<B: Backend> AgzActionModel<B> {
-    fn init(
-        possible_actions: usize,
-        dropout_config: DropoutConfig,
-        device: &B::Device,
-    ) -> AgzActionModel<B> {
-        let dropout = dropout_config.init();
-
+    fn init(possible_actions: usize, device: &B::Device) -> AgzActionModel<B> {
         let channels = possible_actions * PER_ACTION_CHANNELS;
 
         let convs = vec![
@@ -98,7 +78,6 @@ impl<B: Backend> AgzActionModel<B> {
             .collect();
 
         AgzActionModel {
-            dropout,
             convs,
             dense_common,
             dense_per_action,
@@ -119,11 +98,8 @@ impl<B: Backend> AgzActionModel<B> {
             DEEP_WIDTH as i32,
         ]);
 
-        for (i, conv) in self.convs.iter().enumerate() {
+        for conv in self.convs.iter() {
             deep = relu(conv.forward(deep));
-            if i < 3 {
-                deep = self.dropout.forward(deep);
-            }
         }
 
         // Reshape back to vector
@@ -174,10 +150,7 @@ impl<B: Backend> AgzActionModel<B> {
 fn main() {
     let device: Device<Wgpu> = WgpuDevice::DiscreteGpu(0);
 
-    let dropout_config = DropoutConfig { prob: P_DROPOUT };
-
-    let model: AgzActionModel<Wgpu> =
-        AgzActionModel::init(POSSIBLE_ACTIONS, dropout_config, &device);
+    let model: AgzActionModel<Wgpu> = AgzActionModel::init(POSSIBLE_ACTIONS, &device);
 
     for _ in 0..usize::MAX {
         let x: Tensor<Wgpu, 2> = Tensor::random(
